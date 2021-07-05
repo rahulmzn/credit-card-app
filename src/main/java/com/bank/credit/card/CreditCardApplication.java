e7537557eea0ea879c711e7bf19357deeada7c92
Rahul Kumar <rahul.ct.kumar@accenture.com>
2021-07-05 11:40:54 +0100

    Updated application config
    Added server port config to properties file
    Add info logger to show application status

--- src/main/java/com/bank/credit/card/CreditCardApplication.java
+++ src/main/java/com/bank/credit/card/CreditCardApplication.java
@@ -1,13 +1,20 @@
 package com.bank.credit.card;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Spring Boot application, which consume in built tomcat server to run.
+ */
 @SpringBootApplication
 public class CreditCardApplication {
-
+	public static final Logger LOG = LoggerFactory.getLogger(CreditCardApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(CreditCardApplication.class, args);
+		LOG.info("Credit Card Application Started");
 	}
 
 }
