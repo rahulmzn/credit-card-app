77ee32a6c1f0ab84a97cc731611445751ed0946b
Rahul Kumar <rahul.ct.kumar@accenture.com>
2021-07-06 01:46:03 +0100

    Java docs update
    Corrrected java docs

--- src/main/java/com/bank/credit/card/service/CardService.java
+++ src/main/java/com/bank/credit/card/service/CardService.java
@@ -12,13 +12,13 @@ public interface CardService {
 
     /**
      * Perform add card operation for given service, this operation can be used to store new card details in backend system
-     * @param card : Card object which need to be store in backend system
+     * @param card : CreditCardDetailsDto object which need to be store in backend system
      */
     void addCard(CreditCardDetailsDto card);
 
     /**
      * Fetch all stored card from backend system without any filter
-     * @return : List of all available #Card from backend system
+     * @return : List of all available #CreditCardDetailsDto from backend system
      */
     List<CreditCardDetailsDto> getAllCards();
 
