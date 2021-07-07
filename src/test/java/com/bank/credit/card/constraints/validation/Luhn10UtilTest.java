fcc5b1b7436fa2c58cd9b01623936b647cfd5507
Rahul Kumar <rahul.ct.kumar@accenture.com>
2021-07-07 16:11:10 +0100

    Code Improvement with Java8
    Updated luhn10 formula to use java Intstream to improve performance,
    included to use regx as improvement so that this function can be reused for other credit cards format accepted by banks

--- src/test/java/com/bank/credit/card/constraints/validation/Luhn10UtilTest.java
+++ src/test/java/com/bank/credit/card/constraints/validation/Luhn10UtilTest.java
@@ -1,15 +1,11 @@
 package com.bank.credit.card.constraints.validation;
 
 import com.bank.credit.card.utility.Luhn10Util;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 
-import javax.validation.ConstraintValidatorContext;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Validate Card Number Unit tests against Luhn10 Algo
@@ -23,14 +19,15 @@ class Luhn10UtilTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"274637rrr845"})
-    void shouldFailWhenNumberHasOtherThanDigit(String ccNumber) {
-        assertFalse(Luhn10Util.calculateLuhnMod10Check(ccNumber));
+    @ValueSource(strings = {"274637rrr845","6759310784561226","3543-6933-8731-4139","4852 7891 0697 922 0261"})
+    void shouldFailWhenNumberHasOtherThanDigit(String number) {
+        assertFalse(Luhn10Util.calculateLuhnMod10Check(number));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"12345678903555", "012850003580200", "1358954993914435"})
-    void testValidateSuccessWhenValidCardNumberGiven(String ccNumber) {
-        assertTrue(Luhn10Util.calculateLuhnMod10Check(ccNumber));
+    @ValueSource(strings = {"12345678903555", "012850003580200", "1358954993914435", "5277029120773860","4556-0690-9685-2293", "4852789106979220268"})
+    void testValidateSuccessWhenValidCardNumberGiven(String number) {
+        assertTrue(Luhn10Util.calculateLuhnMod10Check(number));
     }
+
 }
