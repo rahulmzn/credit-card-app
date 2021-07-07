bde3345656ff69df403cd35dda68951738131ff5
Rahul Kumar <rahul.ct.kumar@accenture.com>
2021-07-07 16:11:10 +0100

    Code Improvement with Java8
    Updated luhn10 formula to use java Intstream to improve performance,
    included to use regx as improvement so that this function can be reused for other credit cards format accepted by banks

--- src/main/java/com/bank/credit/card/utility/Luhn10Util.java
+++ src/main/java/com/bank/credit/card/utility/Luhn10Util.java
@@ -4,6 +4,8 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.PrimitiveIterator;
+import java.util.stream.IntStream;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Luhn10Util {
@@ -15,23 +17,40 @@ public class Luhn10Util {
      * @return {@code true} if card number pass luhn 10 check result , {@code false} otherwise
      */
     public static boolean calculateLuhnMod10Check(String number) {
-        if(hasOnlyDigits(number)) {
-            int sum = 0;
-            boolean alternate = false;
-            for (int i = number.length() - 1; i >= 0; i--) {
-                int n = Integer.parseInt(number.substring(i, i + 1));
-                if (alternate) {
-                    n *= 2;
-                    if (n > 9) {
-                        n = (n % 10) + 1;
-                    }
-                }
-                sum += n;
-                alternate = !alternate;
-            }
-            return (sum % 10 == 0);
-        }
-        return false;
+        /*
+         * Remove all character other than digits so that if works with combination as 1234-1234-1234-1234, 1234 1234 1234 1234
+         */
+        number = convertToValidDigitsString(number);
+
+        /*
+         * As per Luhn Rule every 1st digit should be multiply by 1 and second should be multiply 2,
+         * Hence factor will have a on demand stream of 1,2,1,2,1,2.
+         */
+        PrimitiveIterator.OfInt factor =
+                IntStream.iterate(1, i -> 3 - i).iterator();
+        /*
+         * 1: Prepare revers string of given number e.g 123456 -> 654321
+         * 2: convert each char in string into int value of that by first map() of IntStream
+         * 3: new use factor with IntStream map to double each second digit of given reversed set of digits.
+         * 4: using reduce it will create a square sum for all these numbers by if has 2 digits then div /10 if single digit then mod by 10
+         * 5: now its check if number mod is 0 or not -> if mod =0 then valid number otherwise invalid number
+         */
+        int sum = new StringBuilder(number).reverse()
+                .toString().chars()
+                .map(c -> c - '0')
+                .map(i -> i * factor.nextInt())
+                .reduce(0, (a, b) -> a + b / 10 + b % 10);
+
+        return (sum % 10) == 0;
+    }
+
+    /**
+     * returns a string after removing character other than digits from string.
+     * @param input : any string with number
+     * @return : string of digits only
+     */
+    private static String convertToValidDigitsString(String input) {
+        return input.replaceAll("[^\\d]", "");
     }
 
 
